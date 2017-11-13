d00d329bdbbb9accaab78e7572baaa69a37419a1
JamesF1260 <33631674+JamesF1260@users.noreply.github.com>
2017-11-13 11:09:41 -0500

    Fist Commit

---
+++ mad lib_jf.py
@@ -0,0 +1,27 @@
+print("Give me a noun")
+noun1 = input()
+
+print("Give me a team sport")
+sport1 = input()
+
+print("Give me a City")
+city1 = input()
+
+print("give me a group of people")
+group = input()
+
+print("Give me a team name")
+team1 = input()
+
+print("give me Another team")
+team2 = input()
+
+print("give me a famouse person")
+person1 = input()
+
+
+print("The " + noun1 + " likes to watch " + sport1)
+print("So he went up to " + city1 + " the stadium was crowded")
+print("Thier was tons of " + group + " they wanted the" + team1 + " team to win")
+print("But, " + noun1 + " wanted the " + team2 + " team to win" )
+print("The fans were excited because " +person1 + " was the team captain for the " + team1 + " team")
